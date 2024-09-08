cffd69bdae0a702402d8e44c02ca79661f3f4d69
pranotipatil01 <166323586+pranotipatil01@users.noreply.github.com>
2024-09-08 20:31:08 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+const navE1=document.querySelector('.navbar');
+window.addEventListener('scroll',()=>{
+    if (window.scrollY >= 50) {
+        navE1.classList.add('navbar-scrolled');
+        
+    }
+    else if(window.screenY < 50){
+            navE1.classList.remove('navbar-scrolled');
+          }
+});
+
+$(function () {
+    $('.counter').counterUp({
+        delay: 100,
+        time: 1000
+    });
+});
